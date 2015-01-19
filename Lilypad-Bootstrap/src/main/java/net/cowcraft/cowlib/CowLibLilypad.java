de39c67da28f0c50b18b3d42e5a806f3acd9b241
BranicYeti <branicyeti@live.co.uk>
2015-01-19 15:26:25 +0000

    Bootstrap modules. Essentially a plugin so you don't have to shade the lib into every project.

---
+++ Lilypad-Bootstrap/src/main/java/net/cowcraft/cowlib/CowLibLilypad.java
@@ -0,0 +1,6 @@
+package net.cowcraft.cowlib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CowLibLilypad extends JavaPlugin
+{}
---
+++ Spigot-Bootstrap/src/main/java/net/cowcraft/cowlib/CowLibSpigot.java
@@ -0,0 +1,6 @@
+package net.cowcraft.cowlib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CowLibSpigot extends JavaPlugin
+{}
